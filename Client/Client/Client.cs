f6b7dd96370c3957275cd16bfea90d04c19627d6
tavisca-acharpe <51903447+tavisca-acharpe@users.noreply.github.com>
2019-07-30 14:35:19 +0530

    final code added

---
+++ Client/Client/Client.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    static class Client
+    {
+        public static void ConnectToServer(IPAddress ipAddress, int portNumber)
+        {
+            
+            var clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            var remoteEndPointConnection = new IPEndPoint(ipAddress, portNumber);
+            try
+            {
+                clientSocket.Connect(remoteEndPointConnection);
+                Console.WriteLine("Connected to {0}", clientSocket.RemoteEndPoint.ToString());
+
+               // Task.Run(()=> RecieveMessage(clientSocket));
+                //     Task.Factory.StartNew(()=> RecieveMessage(clientSocket));
+                //   Task.Factory.StartNew(() => SendMessage(clientSocket));
+
+                var readMessage = new Thread(new ThreadStart(() => RecieveMessage(clientSocket)));
+                var sendMessage = new Thread(new ThreadStart(() => SendMessage(clientSocket))); ;
+                readMessage.Start();
+                sendMessage.Start();
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine("Socket Exception :{0}", se.ToString());
+            }
+        }
+        private static void SendMessage(Socket clientSocket)
+        {
+            while (true)
+            {
+                var message = Console.ReadLine();
+                var messageToBeSent = Encoding.ASCII.GetBytes(message);
+                clientSocket.Send(messageToBeSent);
+            }
+        }
+
+        private static void RecieveMessage(Socket clientSocket)
+        {
+            while (true)
+            {
+                  var messageRecieved = new byte[1024];
+                var byteRecieved = clientSocket.Receive(messageRecieved);
+                Console.WriteLine(Encoding.ASCII.GetString(messageRecieved, 0, byteRecieved));
+            }
+        }
+    }
+}
+
+
+
---
+++ Client/Client/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Net;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client.ConnectToServer(IPAddress.Parse("172.16.5.201"), 8080);
+        }
+    }
+  }
